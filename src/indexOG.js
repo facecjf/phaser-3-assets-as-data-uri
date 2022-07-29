c2230d789887a4898a935e02c8e8ba1cfbbb1c89
facecjf <6617396+facecjf@users.noreply.github.com>
2022-07-29 07:02:08 -0500

    Add files via upload

---
+++ src/indexOG.js
@@ -0,0 +1,136 @@
+import 'phaser';
+
+// import the assets as base-64 data-uri strings with url-loader (see webpack.config.js)
+import blueSrc from '../assets/blue.png';
+import shardsSrc from '../assets/shards.png';
+import sfxSrc from '../assets/sfx.mp3';
+import napieEightFontSrc from '../assets/napie-eight-font.png';
+
+// the json file can be loaded by webpack. url-loader doesn't apply here
+import sfxJson from '../assets/sfx.json';
+
+// this is a small helper to convert the audio (see package.json)
+import toArrayBuffer from 'to-array-buffer';
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 512,
+    height: 512,
+    pixelArt: true,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+var bg;
+var shards;
+var emitter;
+var timedEvent;
+var text;
+
+function preload ()
+{
+    // original loading methods for web (with xhr requests)
+    // ____________________________________________________
+
+    //this.load.image('bg', 'assets/blue.png');
+    //this.load.spritesheet([{ file: 'assets/shards.png', key: 'shards', config: { frameWidth: 16, frameHeight: 16 } }]);
+    //this.load.audioSprite('sfx', ['assets/sfx.ogg', 'assets/sfx.mp3'], 'assets/sfx.json');
+    //this.load.image('napie-eight-font', 'assets/napie-eight-font.png');
+}
+
+function create ()
+{
+    // new methods for loading the assets as data-uri's
+    // adding them directly to the textures and/or cache
+    // We don't place this in the preloader, because
+    // the preloader will immediatly return and
+    // we have our own async stuff and the assets won't
+    // be ready in the create function where we normally
+    // add our gameObjects
+    // _________________________________________________
+
+    var nAssets = 5;
+    var nLoaded = 0; // keep track
+
+    // method for a simple image
+    this.textures.addBase64('bg', blueSrc);
+    nLoaded++;
+
+    // method for a spritesheet
+    var shardsImg = new Image();
+    shardsImg.onload = () => {
+        this.textures.addSpriteSheet('shards', shardsImg, { frameWidth: 16, frameHeight: 16 });
+        // check if assets are ready then call actual phaser create function
+        nLoaded++;
+        if (nLoaded >= nAssets) {
+            var actualCreate = createGameObjects.bind(this);
+            actualCreate();
+        }
+    };
+    shardsImg.src = shardsSrc;
+
+    // method for an audiosprite json file
+    this.cache.json.add('sfx', sfxJson);
+    nLoaded++;
+
+    // method for an audiosprite Audio Buffer
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    audioCtx.decodeAudioData(toArrayBuffer(sfxSrc), (buffer) => {
+        this.cache.audio.add('sfx', buffer);
+        // check if assets are ready then call actual phaser create function
+        nLoaded++;
+        if (nLoaded >= nAssets) {
+            var actualCreate = createGameObjects.bind(this);
+            actualCreate();
+        }
+    }, (e) => { console.log("Error with decoding audio data" + e.err); });
+
+    // method for bitmap font
+    this.textures.addBase64('napie-eight-font', napieEightFontSrc);
+    nLoaded++;
+    var fontConfig = {
+        image: 'napie-eight-font',
+        width: 8,
+        height: 8,
+        chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ .,!?#abcdefghijklmnopqrstuvwxyz@:;^%&1234567890*\'"`[]/\\~+-=<>(){}_|$',
+        charsPerRow: 16,
+        spacing: { x: 0, y: 0 }
+    };
+    this.cache.bitmapFont.add('napie-eight-font', Phaser.GameObjects.RetroFont.Parse(this, fontConfig));
+
+}
+
+// the rest of the code is to check if everything loaded correctly
+// normally we place this in the create function
+function createGameObjects ()
+{
+    bg = this.add.image(256, 256, 'bg');
+    shards = this.add.particles('shards');
+    emitter = shards.createEmitter({
+        frame: [0, 1, 2, 3],
+        x: 200,
+        y: 300,
+        speed: { min: -800, max: 800 },
+        angle: { min: 0, max: 360 },
+        scale: { start: 2, end: 0 },
+        lifespan: 600,
+        gravityY: 800,
+        frequency: -1,
+        rotate: { min: -540, max: 540 }
+    });
+    timedEvent = this.time.addEvent({ delay: 1000, callback: onEvent, callbackScope: this, loop: true });
+    text = this.add.dynamicBitmapText(128, 128, 'napie-eight-font', 'explosions!!!');
+    text.setTint(0xe74c3c);
+    text.setScale(4);
+}
+
+function onEvent ()
+{
+    emitter.explode(8, 128 + Math.floor(Math.random() * 256), 128 + Math.floor(Math.random() * 256));
+    this.sound.playAudioSprite('sfx', 'glass');
+}
