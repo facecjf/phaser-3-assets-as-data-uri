c04dcf5b3093ea058efeff236d5f0f982758f24b
facecjf <6617396+facecjf@users.noreply.github.com>
2023-12-09 11:45:00 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         publicPath: '/build/',
-        filename: 'game.js'
+        filename: 'playable.js'
     },
 
    module: {
